052a0ffcedd8c0f07143dae963375193b7029798
Luis Antonio García Perrusquía <lgarciaperrusquia@gmail.com>
2018-07-25 11:26:34 -0500

    PantallaFiltros

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,13 +2,25 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html'
 })
 export class HomePage {
+	public categoria: string = '';
+	public filtrosData = {
+				materia: '',
+				tema: '',
+				titulo: '',
+				palabraClave: ''
+			};
 
-  constructor(public navCtrl: NavController) {
+	constructor(public navCtrl: NavController) {
 
-  }
+	}
+
+	getFilterData() {
+		console.log('data', this.filtrosData);
+		console.log( 'cat', this.categoria );
+	}
 
 }
