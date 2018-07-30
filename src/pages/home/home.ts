5f34b99a80aeba4bf6b260889f7b18a0e25ae2a4
Luis Antonio García Perrusquía <lgarciaperrusquia@gmail.com>
2018-07-30 18:14:33 -0500

    filtrosData
    Se hacen las peticiones para los idiomas y los temas, se recolectan los datos para hacer la peticion de los resultados

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -14,11 +14,15 @@ import { ResultadosPage } from '../resultados/resultados';
 export class HomePage {
 	private url: string = 'http://coal.com.mx:1100';
 	public categorias: any;
+	public idiomas: any;
+	public temas: any;
+
+	public categoria: any
 	public tema: any;
 	public idioma: any;
 	
 	private filtrosData = {
-				materia: '',
+				descripcion: '',
 				titulo: '',
 				palabraClave: ''
 			};
@@ -26,19 +30,43 @@ export class HomePage {
 	constructor(public navCtrl: NavController, private _http: HttpClient) {	}
 	
 	private _urlCategorias = this.url + "/api/categoria/categorias";
+	private _urlIdiomas = this.url + "/api/idioma/idiomas";
+	private _urlTemas = this.url + "/api/tema/temaByIdCat";
 
 	ionViewDidLoad() {
 		this.getCategorias();
-	}
+		this.getIdiomas();
+	};
 
 	getFilterData() {
-		this.navCtrl.push( ResultadosPage );
-	}
+		console.log( 'titulo', this.filtrosData.titulo );
+		console.log( 'descripcion', this.filtrosData.descripcion );
+		console.log( 'palabraClave', this.filtrosData.palabraClave );
+		console.log( 'categoria', this.categoria );
+		console.log( 'tema', this.tema );
+		console.log( 'idioma', this.idioma );
+		//this.navCtrl.push( ResultadosPage );
+	};
 
 	getCategorias(){
 		let Params = new HttpParams();
 		this._http.get(this._urlCategorias, {params: Params}).subscribe(data => {
 			this.categorias = data;
+		});
+	};
+	
+	getTema(idCategoria){
+		let Params = new HttpParams();
+		Params = Params.append('idCategoria', idCategoria);
+		this._http.get(this._urlTemas, {params: Params}).subscribe(data => {
+			this.temas = data;
+		});
+	};
+
+	getIdiomas(){
+		let Params = new HttpParams();
+		this._http.get(this._urlIdiomas, {params: Params}).subscribe(data => {
+			this.idiomas = data;
 		});
 	};
 };
