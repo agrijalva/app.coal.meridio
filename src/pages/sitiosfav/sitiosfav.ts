d0491de8e35955484c607552f3465cd5f2cc8db2
Luis Antonio García Perrusquía <lgarciaperrusquia@gmail.com>
2018-08-06 18:11:38 -0500

    removeFav
    Se removio de favoritos el enlace

--- src/pages/sitiosfav/sitiosfav.ts
+++ src/pages/sitiosfav/sitiosfav.ts
@@ -12,12 +12,13 @@ export class SitiosfavPage {
 
 	//private url: string = 'http://coal.com.mx:1100';
 	private url: string = 'http://localhost:1100';
+	private urlShare: any = 'http://coal.com.mx:1100/#/link?';
 	public idUsuario: any = 1;
 	public showDiv: number = 0;
 	public favoritos: any;
-
+	
 	constructor(
-		public navCtrl: NavController, 
+		public navCtrl: NavController,
 		public navParams: NavParams,
 		private _http: HttpClient,
 		private alertCtrl: AlertController,
@@ -30,19 +31,23 @@ export class SitiosfavPage {
 
 	ionViewDidLoad() {
 		this.getFavoritos();
-	}
+	};
 
-	private getFavoritos(){
+	private getFavoritos() {
 		let Params = new HttpParams;
-		Params = Params.append( 'idUsuario', this.idUsuario );
+		Params = Params.append('idUsuario', this.idUsuario);
 
-		this._http.get( this.urlGetFav, { params: Params } ).subscribe(data =>{
+		this._http.get(this.urlGetFav, { params: Params }).subscribe(data => {
 			this.favoritos = data;
-			if( this.favoritos.length > 0 ){
+			if (this.favoritos.length > 0) {
 				this.showDiv = 1;
 			}
 		});
 	};
+
+	trackByFav(index) {
+		return index;
+	  }
 
 	starLess(categoria, index) {
 		let Params = new HttpParams;
@@ -50,13 +55,13 @@ export class SitiosfavPage {
 		Params = Params.append('idEnlace', categoria.idEnlace);
 
 		this._http.get(this.urlLessFav, { params: Params }).subscribe(data => {
-			if( data[0].success == 1 ){
+			if (data[0].success == 1) {
 				let alert = this.alertCtrl.create({
 					title: 'Eliminado de favoritos',
 					buttons: ['Listo']
 				});
 				alert.present();
-				this.favoritos[index]['guardado'] = 0;
+				this.getFavoritos();
 			};
 		});
 	};
